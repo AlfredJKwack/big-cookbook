4285b3e0f6c2ce0ea4cdf98ed8bd90918f44f37f
AlfredJKwack <lesparents@PortaMac.local>
2016-07-02 23:10:12 +0200

    check if featured img present
    only fire all the eye candy if necessary

--- js/main.js
+++ js/main.js
@@ -49,57 +49,62 @@ jQuery(document).ready( function($) {
 	// Handle the featured image eye candy
 	//
 	//
-	var $theImage = $('div.featured-img.focuspoint img')[0]
 
-	// fire when image has loaded
-	$('<img />').one('load', function() {
+	if ( $('div.featured-img.focuspoint img').length ) { //check for featured image.
+
+		var $theImage = $('div.featured-img.focuspoint img')[0]
+ 
+		// fire when image has loaded
+		$('<img />').one('load', function() {
 
 
-		// Set background of image to dominant image color
-		//var colorThief = new ColorThief();
-		//var color = colorThief.getColor($theImage);
-		//var rgbValue = 'rgb('+color.join()+')';
-		//var rgbaValue = 'rgba('+color.join()+',0.2)';
-		//$('div.featured-img').css('background-color', rgbValue);
-		//$('#abstract').css('background-color', rgbaValue);
-		
-		
-		//'this' references to the newly created <img />
-		var imgData = {
-			w: this.width,
-			h: this.height			
-		};
+			// Set background of image to dominant image color
+			//var colorThief = new ColorThief();
+			//var color = colorThief.getColor($theImage);
+			//var rgbValue = 'rgb('+color.join()+')';
+			//var rgbaValue = 'rgba('+color.join()+',0.2)';
+			//$('div.featured-img').css('background-color', rgbValue);
+			//$('#abstract').css('background-color', rgbaValue);
+			
+			
+			//'this' references to the newly created <img />
+			var imgData = {
+				w: this.width,
+				h: this.height			
+			};
 
-		// get an appropriate crop window
-		var cropData = SmartCrop.crop(this, {
-				width: 250,		// keeping this slightly smaller than min-height
-				height: 250,
-				minScale: 0.9
-			}, function(result) {
-				return result;
-			}
-		);
-		
-		// get and set a focus point
-		var focusPoint = calcFocus(cropData.topCrop, imgData);
-		$('div.featured-img.focuspoint').attr({
-			"data-focus-x": focusPoint.x,
-			"data-focus-y": focusPoint.y,
-			"data-focus-w": imgData.w,
-			"data-focus-h": imgData.h
-		});
-		$('.focuspoint').focusPoint();
-		
-		// set the text color based on the feature image background
-		BackgroundCheck.init({
-			minComplexity: 16,
-			targets: '#abstract h1, #meta, #brand-link',
-			images: $theImage
-		});		
-		
-		// make the image appear
-		$($theImage).removeClass('is--invisible').hide().fadeIn(1800, 'easeInOutQuad');	
-		
-	}).attr('src', $theImage.src);
+			// get an appropriate crop window
+			var cropData = SmartCrop.crop(this, {
+					width: 250,		// keeping this slightly smaller than min-height
+					height: 250,
+					minScale: 0.9
+				}, function(result) {
+					return result;
+				}
+			);
+			
+			// get and set a focus point
+			var focusPoint = calcFocus(cropData.topCrop, imgData);
+			$('div.featured-img.focuspoint').attr({
+				"data-focus-x": focusPoint.x,
+				"data-focus-y": focusPoint.y,
+				"data-focus-w": imgData.w,
+				"data-focus-h": imgData.h
+			});
+			$('.focuspoint').focusPoint();
+			
+			// set the text color based on the feature image background
+			BackgroundCheck.init({
+				minComplexity: 16,
+				targets: '#abstract h1, #meta, #brand-link',
+				images: $theImage
+			});		
+			
+			// make the image appear
+			$($theImage).removeClass('is--invisible').hide().fadeIn(1800, 'easeInOutQuad');	
+			
+		}).attr('src', $theImage.src);    	
+ 
+	}
 	
 });
