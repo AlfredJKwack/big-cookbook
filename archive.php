0b8e0d97787c41c29a34a737e0b21bb6c72f8619
AlfredJKwack <lesparents@PortaMac.local>
2016-07-03 22:49:49 +0200

    more cosmetics

--- archive.php
+++ archive.php
@@ -47,7 +47,7 @@ get_header(); ?>
 			endwhile;
 
 			?>
-							<a href="#" class="left-menu button trigger">close</a>		
+							<a href="javascript:void(0)" class="left-menu button trigger">close</a>		
 						</div><!-- #blog-list -->
 					</aside><!-- #article_list -->
 			<?php			
