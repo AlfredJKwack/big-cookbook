fec3b4246cd46ab400a828e5245256134b5a21ee
AlfredJKwack <lesparents@PortaMac.local>
2016-07-10 14:44:26 +0200

    Add primary menu to layout
    located on the left fold-out

--- index.php
+++ index.php
@@ -21,21 +21,16 @@ get_header(); ?>
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
 
-				/*
-				if ( $wp_query->current_post == 0 && !is_paged() ) :  
-				*/
 				if ( $wp_query->current_post == 0) : 
 					/* first post */
 					get_template_part( 'template-parts/content-article', get_post_format() );
 
 					?>
 					<aside id="article_list" class="">
+
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+												
 						<h1>More Articles</h1>
 						<div id="blog-list">
 					<?php
