91c588b0c7074069ca6c2fb1108615f7e9c9b979
AlfredJKwack <lesparents@PortaMac.shrinknet>
2017-07-25 00:01:04 +0200

    This is a mess

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -13,6 +13,7 @@
  */
 function big_cookbook_jetpack_setup()
 {
+      
     // Add theme support for Infinite Scroll.
     add_theme_support('infinite-scroll', array(
         'container' => 'blog-list"',
@@ -27,13 +28,60 @@ function big_cookbook_jetpack_setup()
 }
 add_action('after_setup_theme', 'big_cookbook_jetpack_setup');
 
+function big_cookbook_jetpack_custom_support() {
+    $supported = current_theme_supports( 'infinite-scroll' ) && ( is_home() || is_archive() || is_search() || is_singular() );
+     
+    return $supported;
+}
+//add_filter( 'infinite_scroll_archive_supported', 'big_cookbook_jetpack_custom_support' );
+
 /**
  * Custom render function for Infinite Scroll.
  */
 function big_cookbook_infinite_scroll_render()
 {
-    while (have_posts()) {
-        the_post();
-        get_template_part('template-parts/content-thumb', get_post_format());
-    }
+    if ( is_singular() ) :
+    // You'll need the custom loop
+    
+        get_template_part('template-parts/content', 'recent');
+
+    else :
+    // You're good with the normal loop
+        while (have_posts()) {
+            the_post();
+            get_template_part('template-parts/content-thumb', get_post_format());
+        }
+
+    endif;
 }
+
+function big_cookbook_infinite_scroll_switch_to_custom_query(){
+
+    //switch the IS query basis if you're on a singular page.
+    if (is_singular()) {
+        $default_posts_per_page = get_option( 'posts_per_page' );
+
+        // set the number of posts to be the same number shown 
+        // on the 'aside' of the main loop
+        if ($default_posts_per_page > 3 ) : 
+            $default_posts_per_page = $default_posts_per_page - 1;
+        endif;
+
+        if ( get_query_var( 'paged' ) ) { $paged = get_query_var( 'paged' ); }
+        elseif ( get_query_var( 'page' ) ) { $paged = get_query_var( 'page' ); }
+        else { $paged = 1; }
+
+
+        $wp_query = new WP_Query( apply_filters( 'widget_posts_args', array(
+            'posts_per_page'      => $default_posts_per_page,
+            'no_found_rows'       => true,
+            'post_status'         => 'publish',
+            'ignore_sticky_posts' => true,
+            'paged'               => $paged
+        ) ) );
+        if ($wp_query->have_posts()) :
+            add_filter ( 'infinite_scroll_query_object', $wp_query );
+        endif;  
+    };
+}
+//add_action( 'the_post', 'big_cookbook_infinite_scroll_switch_to_custom_query' );
--- template-parts/content-recent.php
+++ template-parts/content-recent.php
@@ -8,16 +8,35 @@
 
 $default_posts_per_page = get_option( 'posts_per_page' );
 
-$r = new WP_Query( apply_filters( 'widget_posts_args', array(
+// set the number of posts to be the same number shown 
+// on the 'aside' of the main loop
+if ($default_posts_per_page > 3 ) : 
+	$default_posts_per_page = $default_posts_per_page - 1;
+endif;
+
+if ( get_query_var( 'paged' ) ) { $paged = get_query_var( 'paged' ); }
+elseif ( get_query_var( 'page' ) ) { $paged = get_query_var( 'page' ); }
+else { $paged = 1; }
+
+
+$wp_query = new WP_Query( apply_filters( 'widget_posts_args', array(
 	'posts_per_page'      => $default_posts_per_page,
 	'no_found_rows'       => true,
 	'post_status'         => 'publish',
-	'ignore_sticky_posts' => true
+	'ignore_sticky_posts' => true,
+	'paged'               => $paged
 ) ) );
 
-if ($r->have_posts()) :
 
-	while ( $r->have_posts() ) : $r->the_post();
+if ($wp_query->have_posts()) :
+
+	// Jetpack integration: change its base query reference
+	// if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'infinite-scroll' ) ) :
+	// 	add_filter ( 'infinite_scroll_query_object', $wp_query );
+	// 	?>  <!-- HELLO --> <?php
+	// endif;	
+
+	while ( $wp_query->have_posts() ) : $wp_query->the_post();
 
 		get_template_part('template-parts/content-thumb', get_post_format());
 
@@ -25,6 +44,11 @@ if ($r->have_posts()) :
 
 	// Reset the global $the_post as this query will have stomped on it
 	wp_reset_postdata();
+
+	 // Jetpack integration: change its base query reference
+	// if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'infinite-scroll' ) ) :
+	// 	add_filter ( 'infinite_scroll_query_object', $GLOBALS['wp_the_query'] );
+	// endif;	
 
 endif;	
 
